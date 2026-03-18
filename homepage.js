d7206905cd8cc4ca3525bf25d2e9dff2ffa100b5
Dabble <hardabrahma7@gmail.com>
2026-03-18 01:20:02 +0100

    Updated homepage.js

---
+++ homepage.js
@@ -0,0 +1,14 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+    const username = document.getElementById("usernameInput").value.trim();
+
+    if (username.length === 0) {
+        alert("Please enter a name");
+        return;
+    }
+
+    // Save username so the game page can read it
+    localStorage.setItem("username", username);
+
+    // Redirect to your game page
+    window.location.href = "index.html";
+});
